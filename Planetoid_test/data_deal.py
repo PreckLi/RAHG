6eb4f2a6377d5c8b2f891fde92b415715f786d3a
Li Kunhao <46922020+PreckLi@users.noreply.github.com>
2024-07-05 20:08:05 +0800

    Update data_deal.py

--- Planetoid_test/data_deal.py
+++ Planetoid_test/data_deal.py
@@ -13,9 +13,8 @@ def load_data(args):
     emb_stru_file = f'datasets/{args.dataset}/node2vec_emb/{args.dataset}.emb'
     x_role, num_nodes, num_node_features_r = get_X_matrix(emb_role_file, 'graphwave')
     x_stru, _, num_node_features_s = get_X_matrix(emb_stru_file, 'node2vec')
-    edge = to_dense_adj(dataset.data.edge_index).squeeze(0)
-    data.x_role = torch.cat((dataset.data.x, x_role ,edge), dim=1)
-    data.x_stru = torch.cat((dataset.data.x,  x_stru,edge), dim=1)
-    data.num_node_features_r = num_node_features_r + dataset.num_node_features + num_nodes
-    data.num_node_features_s = num_node_features_s + dataset.num_node_features + num_nodes
+    data.x_role = x_role
+    data.x_stru = x_stru
+    data.num_node_features_r = num_node_features_r
+    data.num_node_features_s = num_node_features_s
     return data
